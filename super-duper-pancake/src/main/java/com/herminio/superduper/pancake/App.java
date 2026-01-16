e7d4fa17cda3a335dae1837868d3c482263a954f
Hermínio <herminio.aureo.neto@accenture.com>
2026-01-16 14:31:51 -0300

    Fix reserved keyword 'super' in package name by renaming to superduper

---
+++ super-duper-pancake/src/main/java/com/herminio/superduper/pancake/App.java
@@ -0,0 +1,13 @@
+package com.herminio.superduper.pancake;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
